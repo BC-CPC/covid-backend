40d34743f8498fa03d4f7eccee6f6683471e9b7d
Mazen Kotb <me@mazenk.com>
2020-04-23 21:40:44 -0700

    Make results unique by name

--- src/main/kotlin/ca/bccpc/covid/routes/SearchController.kt
+++ src/main/kotlin/ca/bccpc/covid/routes/SearchController.kt
@@ -1,6 +1,7 @@
 package ca.bccpc.covid.routes
 
 import ca.bccpc.covid.data.CriteriaType
+import ca.bccpc.covid.data.Resource
 import ca.bccpc.covid.data.ResourceSheet
 import io.javalin.Context
 
@@ -8,7 +9,7 @@ object SearchController {
     fun search(context: Context) {
         val request = context.bodyAsClass(SearchRequest::class.java)
 
-        context.json(ResourceSheet.findResources(request.query))
+        context.json(ResourceSheet.findResources(request.query).distinctBy(Resource::name))
     }
 }
 
